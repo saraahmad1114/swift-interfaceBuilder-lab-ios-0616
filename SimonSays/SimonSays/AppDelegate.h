380ab309cc15a3c047530e27dfe57418df510d99
Sara <saraahmad1114@yahoo.com>
2016-07-08 13:13:58 -0400

    simon says

---
+++ SimonSays/SimonSays/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimonSays
+//
+//  Created by Flatiron School on 7/8/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimonSays/SimonSays/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimonSays
+//
+//  Created by Flatiron School on 7/8/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
